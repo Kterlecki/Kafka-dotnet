af61c8a617c5c6717752c1cdbef1890c27a79775
Kterlecki <terlecki1995@gmail.com>
2023-07-22 21:30:47 +0100

    WIP - building producer

--- src/Controllers/ProducerController.cs
+++ src/Controllers/ProducerController.cs
@@ -1,4 +1,7 @@
+using System.Net;
+using System.Text.Json;
 using Confluent.Kafka;
+using kafka_dotnet.Models;
 using Microsoft.AspNetCore.Mvc;
 namespace kafka_dotnet.Controllers;
 
@@ -6,5 +9,20 @@ namespace kafka_dotnet.Controllers;
 [ApiController]
 public class ProducerController : ControllerBase
 {
-    
+    private readonly string bootstrapServer = "localhost:9092";
+    private readonly string topic = "quickstart-events";
+
+    [HttpPost]
+    public async Task <IActionResult> Post([FromBody] OrderRequest orderRequest)
+    {
+        string message = JsonSerializer.Serialize(orderRequest);
+        return Ok(await SendOrderRequest(topic, message));
+    }
+    private async Task <bool> SendOrderRequest(string topic, string message)
+    {
+        ProducerConfig config  = new ProducerConfig{
+            BootstrapServers = bootstrapServer,
+            ClientId = Dns.GetHostName()
+        };
+    }
 }
