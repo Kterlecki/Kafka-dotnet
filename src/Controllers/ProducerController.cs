74463ce429459dd0d1d5c517f14a79ec0a9590ff
Kterlecki <terlecki1995@gmail.com>
2023-07-23 20:06:48 +0100

    Completed initial ProducerController for Kafka

--- src/Controllers/ProducerController.cs
+++ src/Controllers/ProducerController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
 using Confluent.Kafka;
@@ -13,16 +14,31 @@ public class ProducerController : ControllerBase
     private readonly string topic = "quickstart-events";
 
     [HttpPost]
-    public async Task <IActionResult> Post([FromBody] OrderRequest orderRequest)
+    public async Task<IActionResult> Post([FromBody] OrderRequest orderRequest)
     {
         string message = JsonSerializer.Serialize(orderRequest);
         return Ok(await SendOrderRequest(topic, message));
     }
-    private async Task <bool> SendOrderRequest(string topic, string message)
+    private async Task<bool> SendOrderRequest(string topic, string message)
     {
         ProducerConfig config  = new ProducerConfig{
             BootstrapServers = bootstrapServer,
             ClientId = Dns.GetHostName()
         };
+
+        try
+        {
+            using(var producer = new ProducerBuilder<Null, string>(config).Build()){
+                var result = await producer.ProduceAsync(topic, new Message<Null, string> {
+                    Value = message
+                });
+            Debug.WriteLine($"Delivery Timestamp: {result.Timestamp.UtcDateTime}");
+            return await Task.FromResult(true);
+            }
+        } catch (Exception ex)
+        {
+            Console.WriteLine($"Error occured: {ex.Message}");
+        }
+        return await Task.FromResult(false);
     }
 }
